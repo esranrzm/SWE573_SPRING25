f87152df5bdd20719714f4bc9ae025b6b9ac66b1
esranrzm <esranurozumm@gmail.com>
2025-03-02 22:30:22 +0300

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import "../styles/Navbar.css";
+import { Link, NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav>
+      <Link to="/" className="title">
+        Website
+      </Link>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/profile">Profile</NavLink>
+        </li>
+        <li>
+          <NavLink to="/logout">Logout</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
