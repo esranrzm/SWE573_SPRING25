6aa2ef06183598ca8bae56e0b11e87b3f54cc355
esra nur özüm <84041867+esranrzm@users.noreply.github.com>
2025-05-07 14:21:46 +0300

    delete duplicated navigate

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,6 @@ const Login = () => {
     const [visible, setVisible] = useState(false);
     const navigate = useNavigate();
     const getUrlPrefix = ConfigHelper.getItem("url");
-    const navigate = useNavigate();
     const logInUser = async () => {
 
         try {
